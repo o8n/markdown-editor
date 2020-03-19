8785808f371741480f1c1609850447229113fabd
okamotchan <m.okamotchan@gmail.com>
2020-03-20 02:10:54 +0900

    add remark-react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import unified from 'unified'
+import parse from 'remark-parse'
+import remark2react from 'remark-react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = { text: '# hello world' }
+    this.onChange = this.onChange.bind(this)
+  }
+  onChange(e) {
+    this.setState({ text: e.target.value })
+  }
+  render() {
+    return (
+      <div>
+        <textarea value={this.state.text} onChange={this.onChange} />
+        <div id="preview">
+          {
+            unified()
+              .use(parse)
+              .use(remark2react)
+              .processSync(this.state.text).contents
+          }
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
