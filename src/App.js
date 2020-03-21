0cb7b3bf5c31aaf79a6c22f7f681df20e45b07d2
okamotchan <m.okamotchan@gmail.com>
2020-03-21 15:26:53 +0900

    make view with remark

--- src/App.js
+++ src/App.js
@@ -1,33 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import unified from 'unified'
-import parse from 'remark-parse'
-import remark2react from 'remark-react'
+import React from 'react';
+import './App.css';
+import Top from './view/Top';
 
-class App extends React.Component {
-  constructor() {
-    super()
-    this.state = { text: '# hello world' }
-    this.onChange = this.onChange.bind(this)
-  }
-  onChange(e) {
-    this.setState({ text: e.target.value })
-  }
-  render() {
-    return (
-      <div>
-        <textarea value={this.state.text} onChange={this.onChange} />
-        <div id="preview">
-          {
-            unified()
-              .use(parse)
-              .use(remark2react)
-              .processSync(this.state.text).contents
-          }
-        </div>
-      </div>
-    )
-  }
+const App = () => {
+  return (
+    <div className='App'>
+      <Top/>
+    </div>
+  );
 }
 
 export default App;
--- src/view/Top.js
+++ src/view/Top.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import exEdit from '../component/exEdit';
 
 const Top = () => {
   return (
     <div>
-      
+      <exEdit/>
     </div>
   )
 }
